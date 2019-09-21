1612c2c4dea9ff3b487a5d262b2c5f272417b21c
TaeYeongHWANG <taeyeong.hwang@schoolsharing.co.kr>
2019-09-21 14:50:07 +0900

    Apply gitignore

--- app.js
+++ app.js
@@ -8,4 +8,28 @@ const Header = () => (
 	</header>
 );
 
-ReactDOM.render(<Header/>, document.getElementById('root'));
+const Counter = () => (
+	<div className="counter">
+		<button className="counter-action decrement"> -</button>
+		<span className="counter-score">35</span>
+		<button className="counter-action increment"> +</button>
+	</div>
+);
+
+const Player = () => (
+	<div className="player">
+		<span className="player-name">LDK</span>
+		<Counter/>
+	</div>
+);
+
+const App = () => (
+	<div className="scoreboard">
+		<Header/>
+
+		<Player/>
+	</div>
+);
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
