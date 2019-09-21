e72464495ffee9f6f2582399963115cac365b431
TaeYeongHWANG <taeyeong.hwang@schoolsharing.co.kr>
2019-09-21 16:06:47 +0900

    map & props

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
+const players = [
+	{name: "KYH", score: 30, id: 1},
+	{name: "HTY", score: 40, id: 2},
+	{name: "KMG", score: 50, id: 3},
+	{name: "JDI", score: 60, id: 4}
+];
 // function component 만드는 법
 // 1. 대문자로 시작
 // 2. React Element를 리턴
-const Header = () => (
-	<header className="header">
-		<h1 className="h1">Scoreboard</h1>
-		<span className="stats">Players: 1</span>
-	</header>
-);
+const Header = (props) => {
+	console.log(props);
+	return (
+		<header className="header">
+			<h1 className="h1">{props.title}</h1>
+			<span className="stats">{props.totalPlayers}</span>
+		</header>
+	);
+}
 
-const Counter = () => (
-	<div className="counter">
-		<button className="counter-action decrement"> -</button>
-		<span className="counter-score">35</span>
-		<button className="counter-action increment"> +</button>
-	</div>
-);
+const Counter = (props) => {
+	return (
+		<div className="counter">
+			<button className="counter-action decrement"> -</button>
+			<span className="counter-score">{props.score}</span>
+			<button className="counter-action increment"> +</button>
+		</div>
+	);
+}
 
-const Player = () => (
-	<div className="player">
-		<span className="player-name">LDK</span>
-		<Counter/>
-	</div>
-);
+const Player = (props) => {
+	return (
+		<div className="player">
+			<span className="player-name">{props.name}</span>
+			<Counter score={props.score}/>
+		</div>
+	);
+}
 
-const App = () => (
-	<div className="scoreboard">
-		<Header/>
+const App = (props) => {
+	return (
+		<div className="scoreboard">
+			<Header title="My Scoreboard" totalPlayers={11} />
 
-		<Player/>
-	</div>
-);
+			{ // JSX Expression
+				props.initPlayers.map((player) => { // Function Expression, Don't type semicolon (;)
+					return ( // JSX 리턴 해야하므로 JSX Expression 사용
+						<Player name={player.name} score={player.score} key={player.id} />
+					)
+				})
+			}
 
+			{ // Hard cording
+				// [
+				// 	<Player name="KMG" score={80} />,
+				// 	<Player name="KMG" score={80} />,
+				// 	<Player name="KMG" score={80} />,
+				// 	<Player name="KMG" score={80} />
+				// ]
+			}
+		</div>
+	);
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App initPlayers={players}/>, document.getElementById('root'));
